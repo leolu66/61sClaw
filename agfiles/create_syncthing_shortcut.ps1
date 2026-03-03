432dc620d4df42c82ae23b3941f6194d7c6034ff
leoray <33197620+leolu66@users.noreply.github.com>
2026-03-03 13:45:13 +0800

    chore: 忽略整个 agfiles 目录

--- agfiles/create_syncthing_shortcut.ps1
+++
@@ -1,6 +0,0 @@
-$ws = New-Object -ComObject WScript.Shell
-$s = $ws.CreateShortcut("$env:USERPROFILE\Desktop\Syncthing.lnk")
-$s.TargetPath = "E:\download\syncthing\syncthing-windows-amd64-v1.27.7\syncthing.exe"
-$s.WorkingDirectory = "E:\download\syncthing\syncthing-windows-amd64-v1.27.7"
-$s.Description = "Syncthing 文件同步"
-$s.Save()
